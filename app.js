26196e7819b3944db375fe7c8514dd81753efd03
CarolPinerosTrujillo <caroll25m@gmail.com>
2026-07-15 13:05:35 -0500

    Funcionamiento ok, pendiente personalizacion

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 //ARCHIVO JS
+
+let contador = 0;
+
+function actualizarColor() {
+    const elemento = document.getElementById("contador");
+
+    if (contador >= 10) {
+        elemento.style.color = "red";
+    } else {
+        elemento.style.color = "black";
+    }
+}
+
+function incrementar() {
+    contador++;
+    document.getElementById("contador").innerText = contador;
+    actualizarColor();
+}
+
+function decrementar() {
+    contador--;
+    document.getElementById("contador").innerText = contador;
+    actualizarColor();
+}
